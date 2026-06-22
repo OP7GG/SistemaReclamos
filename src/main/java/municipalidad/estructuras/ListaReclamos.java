c23b559e387bd9bf55d791daaba49428fb710ded
cristian <hokahe600@gmail.com>
2026-06-21 22:03:32 -0400

    agregar clase listareclamos

---
+++ src/main/java/municipalidad/estructuras/ListaReclamos.java
@@ -0,0 +1,99 @@
+package municipalidad.estructuras;
+
+import municipalidad.modelo.Reclamo;
+
+public class ListaReclamos {
+    
+    private Nodo cabeza;
+    private int tamaño;
+    
+    private class Nodo {
+        Reclamo dato;
+        Nodo siguiente;
+        Nodo(Reclamo dato) {
+            this.dato = dato;
+            this.siguiente = null;
+        }
+    }
+    
+    public ListaReclamos() {
+        cabeza = null;
+        tamaño = 0;
+    }
+    
+    public void agregar(Reclamo reclamo) {
+        Nodo nuevo = new Nodo(reclamo);
+        if (cabeza == null) {
+            cabeza = nuevo;
+        } else {
+            Nodo actual = cabeza;
+            while (actual.siguiente != null) {
+                actual = actual.siguiente;
+            }
+            actual.siguiente = nuevo;
+        }
+        tamaño++;
+    }
+    
+    public boolean eliminar(String codigo) {
+        if (cabeza == null) {
+            return false;
+        }
+        
+        if (cabeza.dato.getCodigo().equals(codigo)) {
+            cabeza = cabeza.siguiente;
+            tamaño--;
+            return true;
+        }
+        
+        Nodo actual = cabeza;
+        while (actual.siguiente != null) {
+            if (actual.siguiente.dato.getCodigo().equals(codigo)) {
+                actual.siguiente = actual.siguiente.siguiente;
+                tamaño--;
+                return true;
+            }
+            actual = actual.siguiente;
+        }
+        
+        return false;
+    }
+    
+    public Reclamo buscar(String codigo) {
+        Nodo actual = cabeza;
+        while (actual != null) {
+            if (actual.dato.getCodigo().equals(codigo)) {
+                return actual.dato;
+            }
+            actual = actual.siguiente;
+        }
+        return null;
+    }
+    
+    public Reclamo[] toArray() {
+        Reclamo[] arr = new Reclamo[tamaño];
+        Nodo actual = cabeza;
+        int i = 0;
+        
+        while (actual != null) {
+            arr[i] = actual.dato;
+            actual = actual.siguiente;
+            i++;
+        }
+        
+        return arr;
+    }
+    
+    public int getTamaño() {
+        return tamaño;
+    }
+    
+    public void mostrar() {
+        Nodo actual = cabeza;
+        System.out.println("Todos los reclamos");
+        while (actual != null) {
+            System.out.println(actual.dato);
+            actual = actual.siguiente;
+        }
+    }
+}
